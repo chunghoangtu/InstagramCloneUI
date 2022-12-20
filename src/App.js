3b10c0aa8182f104d2c426a4b97d089fb460a115
DESKTOP-CNDF47Q\chunghoangtu <chunghoangtu@gmail.com>
2022-12-20 22:12:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div className='icu-app'>
+      <nav className="icu-side-nav"></nav>
+      <main className="icu-main">
+        <section className="icu-wall">
+          <div className="icu-wall__stories"></div>
+          <div className="icu-wall__posts"></div>
+        </section>
+        <section className="icu-user-info">
+        </section>
+      </main>
+    </div>
   )
 }
